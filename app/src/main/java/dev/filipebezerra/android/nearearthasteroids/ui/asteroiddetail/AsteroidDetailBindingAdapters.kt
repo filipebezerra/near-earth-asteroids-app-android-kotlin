8f84fda9f670c9ac03700cb0b63fb11cd944dbf3
Filipe Bezerra <filipebzerra@gmail.com>
2020-12-16 11:28:47 -0300

    refactor: Move BindingAdapters to the file they belong

--- app/src/main/java/dev/filipebezerra/android/nearearthasteroids/ui/asteroiddetail/AsteroidDetailBindingAdapters.kt
+++ app/src/main/java/dev/filipebezerra/android/nearearthasteroids/ui/asteroiddetail/AsteroidDetailBindingAdapters.kt
@@ -6,46 +6,13 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import dev.filipebezerra.android.nearearthasteroids.R
 import dev.filipebezerra.android.nearearthasteroids.domain.CloseApproachData
 import dev.filipebezerra.android.nearearthasteroids.domain.EstimatedDiameter
-import dev.filipebezerra.android.nearearthasteroids.domain.PictureOfDay
 import dev.filipebezerra.android.nearearthasteroids.util.ext.LocalDateExt.dateNow
 import dev.filipebezerra.android.nearearthasteroids.util.ext.asIsoLocalDate
 import dev.filipebezerra.android.nearearthasteroids.util.ext.toEpochMilli
-
-@BindingAdapter("pictureOfDay", "bitmap")
-fun ImageView.bindPictureOfDay(
-    pictureOfDay: PictureOfDay?,
-    bitmap: Boolean? = false,
-) = pictureOfDay?.apply {
-    when (bitmap) {
-        true -> Glide.with(context)
-            .asBitmap()
-            .load(hdPictureUrl)
-            .transition(BitmapTransitionOptions.withCrossFade())
-        else -> Glide.with(context)
-            .load(pictureOfDay)
-            .transition(DrawableTransitionOptions.withCrossFade())
-    }.apply {
-        placeholder(R.drawable.picture_of_day_placeholder)
-        error(R.drawable.picture_of_day_placeholder)
-        fallback(R.drawable.picture_of_day_placeholder)
-        contentDescription = if (hdPictureUrl == null) context.getString(R.string.picture_of_day_placeholder) else title
-    }.run { into(this@bindPictureOfDay) }
-}
-
-@BindingAdapter("titlePictureOfDay")
-fun CollapsingToolbarLayout.bindTitleOfPictureOfDay(pictureOfDay: PictureOfDay?) =
-    pictureOfDay?.let { picture ->
-        title = when {
-            picture.hdPictureUrl != null -> picture.title
-            else -> context.getString(R.string.picture_of_day_placeholder)
-        }
-    }
 
 @BindingAdapter("asteroidHazardousOrSafe")
 fun ImageView.bindAsteroidHazardousOrSafe(isPotentiallyHazardousAsteroid: Boolean) =
--- app/src/main/java/dev/filipebezerra/android/nearearthasteroids/ui/asteroidlist/AsteroidListBindingAdapters.kt
+++ app/src/main/java/dev/filipebezerra/android/nearearthasteroids/ui/asteroidlist/AsteroidListBindingAdapters.kt
@@ -1,12 +1,49 @@
 package dev.filipebezerra.android.nearearthasteroids.ui.asteroidlist
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import dev.filipebezerra.android.nearearthasteroids.R
 import dev.filipebezerra.android.nearearthasteroids.domain.Asteroid
+import dev.filipebezerra.android.nearearthasteroids.domain.PictureOfDay
 
 @BindingAdapter("asteroidList")
 fun bindAsteroidList(listView: RecyclerView, asteroids: List<Asteroid>?) =
     asteroids?.let { (listView.adapter as AsteroidListAdapter).submitList(asteroids) }
+
+@BindingAdapter("pictureOfDay", "bitmap")
+fun ImageView.bindPictureOfDay(
+    pictureOfDay: PictureOfDay?,
+    bitmap: Boolean? = false,
+) = pictureOfDay?.apply {
+    when (bitmap) {
+        true -> Glide.with(context)
+            .asBitmap()
+            .load(hdPictureUrl)
+            .transition(BitmapTransitionOptions.withCrossFade())
+        else -> Glide.with(context)
+            .load(pictureOfDay)
+            .transition(DrawableTransitionOptions.withCrossFade())
+    }.apply {
+        placeholder(R.drawable.picture_of_day_placeholder)
+        error(R.drawable.picture_of_day_placeholder)
+        fallback(R.drawable.picture_of_day_placeholder)
+        contentDescription = if (hdPictureUrl == null) context.getString(R.string.picture_of_day_placeholder) else title
+    }.run { into(this@bindPictureOfDay) }
+}
+
+@BindingAdapter("titlePictureOfDay")
+fun CollapsingToolbarLayout.bindTitleOfPictureOfDay(pictureOfDay: PictureOfDay?) =
+    pictureOfDay?.let { picture ->
+        title = when {
+            picture.hdPictureUrl != null -> picture.title
+            else -> context.getString(R.string.picture_of_day_placeholder)
+        }
+    }
 
 /*
 @BindingAdapter("apiCallStatus")
